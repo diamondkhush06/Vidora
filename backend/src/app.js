4ae252124b5f2c26196f1795fc040505103b7fed
khushi rokde <khushirokdejee@gmail.com>
2025-08-07 19:13:26 +0530

    Ready for deployment

---
+++ backend/src/app.js
@@ -0,0 +1,40 @@
+import express from "express";
+import { createServer } from "node:http"; // conects socket ka server and express ka server
+
+import { Server } from "socket.io";
+
+import mongoose from "mongoose";
+import { connectToSocket } from "./controllers/socketManager.js";
+
+import cors from "cors";
+import userRoutes from "./routes/users.routes.js";
+
+const app = express();
+const server = createServer(app);
+const io = connectToSocket(server);
+
+
+app.set("port", (process.env.PORT || 8000))
+app.use(cors());
+app.use(express.json({ limit: "40kb" }));
+app.use(express.urlencoded({ limit: "40kb", extended: true }));
+
+app.use("/api/v1/users", userRoutes);
+
+const start = async () => {
+    app.set("mongo_user")
+    const connectionDb = await mongoose.connect("mongodb+srv://khushirokdejee:Khushi2024@cluster0.fp2u2so.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+
+
+    console.log(`MONGO Connected DB HOst: ${connectionDb.connection.host}`)
+    server.listen(app.get("port"), () => {
+        console.log("LISTENIN ON PORT 8000")
+    });
+
+
+
+}
+
+
+
+start();
